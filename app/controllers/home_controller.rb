3783ef0f8019c142d1a2a847155b818cbacc3e67
wandji20 <wbertrand10@gmail.com>
2025-04-08 14:34:57 +0100

    Render books and add search form, js controller and filter records

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,13 @@
 class HomeController < ApplicationController
   def books
-    @books = []
+    query = params[:query].strip
+    @books = if query.present?
+      q_string = "%#{Book.sanitize_sql_like(query)}%"
+      Book.where("title ILIKE ? OR author ILIKE ?", q_string, q_string)
+    else
+      Book.all
+    end
+
+    @books.order(:title, :author)
   end
 end
